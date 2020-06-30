030bba1dae3a0854753327b2324af21c3f4e08b7
Arvick <macalaladjustin@gmail.com>
2020-06-29 18:38:30 -0700

    Draft3
    Now with dobre brothers

--- Markov.py
+++ Markov.py
@@ -1,27 +1,25 @@
 import random
+forbidden=[",",":",";","?","!","-","/","&","#","@","*",".",'"',"_","(",")","-","“","”","-","[","]"]
 prime=[]
-forbidden=[",",":",";","?","!","-","/","&","#","@","*",".",'"',"_","(",")","-","“","”","-"]
-with open('x.txt',encoding="utf8") as obj:
+with open('x.txt') as obj:
     temp=obj.readlines()
     for i in temp:
         prime.append(i.strip())
     prime=[i for i in prime if i!='']
-newp=[]
-for i in prime:
-    temp=[letter for letter in i]
-    for n in temp:
-        if n in forbidden:
-            temp.remove(n)
-    newp.append("".join(temp))
-newp2=[]
-for i in newp:
-    temp=[letter for letter in i]
-    for n in temp:
-        if n in forbidden:
-            temp.remove(n)
-    newp2.append("".join(temp))
+
+for char in forbidden:
+    while char in "".join(prime):
+        temple=[]
+        for i in prime:
+            temp=[letter for letter in i]
+            for n in temp:
+                if n in forbidden:
+                    temp.remove(n)
+            temple.append("".join(temp))
+        prime=temple
+
 sent={}
-for i in newp2:
+for i in prime:
     a=i.lower().split()
     count=0
     while count<len(a)-1:
@@ -33,4 +31,28 @@ for i in newp2:
             else:
                 sent[a[count]][a[count+1]]=1
         count+=1
-print(sent)
+
+def generate(num,dict):
+    tem=[]
+    keys=list(dict.keys())
+    values=list(dict.values())
+    tem.append(random.choice(keys))
+    while len(tem)<num:
+      if tem[-1] not in keys:
+        tem.append(random.choice(keys))
+      else:
+        valuedict=dict[tem[-1]]
+        tem.append(random.choice(list(valuedict.keys())))
+      if tem[-1]==tem[-2]:
+        tem.append(random.choice(keys))
+    for i in tem:
+      i.capitalize()
+    return(" ".join(tem))
+
+      
+
+
+for i in range(1,41):
+  print(generate(random.randrange(1,101),sent),"\n")
+#for key,value in sent.items():
+ # print(key,value)
