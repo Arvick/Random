671bb8af3d7f2d9ebf5f5d462d426476a81d596a
Arvick <macalaladjustin@gmail.com>
2020-07-03 21:19:09 -0700

    Update

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,4 +1,4 @@
-def fibonacci(startnum=1,num=10):
+def fibonacci(startnum=1,num=20):
     fib=[1,1]
     while len(fib)<num:
         fib.append(fib[-1]+fib[-2])
@@ -6,9 +6,6 @@ def fibonacci(startnum=1,num=10):
     try:
         fib=fib[fib.index(str(startnum)):]
     except ValueError:
-        return('This starting number is not in the Fibonacci Sequence.')
+        return('The number is not in the Fibonacci Sequence.')
     return("\n".join(fib))
-
-
-
-print(fibonacci(3,10))
+print(fibonacci())
