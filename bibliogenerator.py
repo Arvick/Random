c7b4ce2b1b763e17488a8c7e531db89da99a224a
Arvick <macalaladjustin@gmail.com>
2020-08-15 14:31:36 -0700

    Update 8/15/2020
    Added Protests & Everything's A Cure (Not)
    
    Refined bibliography script (more automated).

--- bibliogenerator.py
+++ bibliogenerator.py
@@ -1,11 +1,24 @@
+x=int(input("How many lines?"))
+
+
 a=[]
 #put links in a
+def quote(num):
+  temp=0
+  while temp<x:
+    y=input("Enter line here: ")
+    a.append(str(y))
+    temp+=1
+  return("Finished.")
+
+quote(x)
 
 def generate(lis):
   b=[]
   for i in lis:
     b.append(f"<li><a href='{i}'>{i}</a></li>")
   return(b)
+
 
 c=generate(a)
 
