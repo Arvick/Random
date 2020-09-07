81734d508959c513c24948d5730c0d8f5fc83a07
Arvick <macalaladjustin@gmail.com>
2020-09-07 13:22:29 -0700

    Update 9/7/2020

--- imagebiblio.py
+++ imagebiblio.py
@@ -1,18 +1,25 @@
 linklist=[]
+#appends each link to be used
 with open('links.txt', 'r') as links:
     for line in links.readlines():
         linklist.append(line)
 
+#formats and appends links to master
 master=[]
 def formt():
     for i in linklist:
         alt=input(f'''Current Link: {i}. What is the alternate text? 
-                  ''')
+        ''')
         master.append(f"<li><a href='{i}'> {alt}</a></li>")
 
 formt()
 
+#prints out each entry
 for i in master:
     print(i)
+
+#clears text file
+with open('links.txt', 'w') as links:
+    links.close
 
         
